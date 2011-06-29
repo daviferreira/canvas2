1b41c6d41baf8652e629dd80b75616aaecf962e9
Davi Ferreira <contato@daviferreira.com>
2011-06-29 16:00:47 -0300

    First rewriting.

--- canvas.php
+++ canvas.php
@@ -1,38 +1,63 @@
 <?php
+require_once dirname(__FILE__).'/imagecreatefrombmp.php';
+
 class canvas{
 
   private $file, $image, $temp_image;
 
   private $width, $height, $new_width, $new_height, $html_size;
 
-  private $format, $extension, $size, $file, $directory;
+  private $format, $extension, $size, $html_size, $basename, $dirname;
 
   private $rgb = array(255, 255, 255);
 
   private $crop_coordinates;
 
+  private $error;
+
   public function __construct($file = null){
-  
+    if($file){
+      $this->file = $file;
+      $this->imageinfo();
+    } 
   }
 
   private function image_info(){
-  
+    if(is_file($this->file)){
+      $this->file_info();
+      if(!$this->is_image())
+        $this->error = "Invalid file. {$this->file} is not an image file.";
+      else
+        $this->dimensions()->create_image();
+    }else{
+      $this->error = "File not accessible/found.";
+    }
   }
 
   public function load($file){
-  
+    $this->file = $file;
+    $this->image_info();
+    return $this;
   }
 
   private function dimensions(){
-  
+    list($this->width, $this->height, $this->html_size, $this->format) = getimagesize($this->file);
+    return $this; 
   }
 
   private function file_info(){
-  
+    $pathinfo = pathinfo($this->file);
+    $this->extension = strtolower($pathinfo['extension']);
+    $this->basename = $pathinfo['basename'];
+    $this->dirname = $pathinfo['dirname'];
   }
 
   private function is_image(){
-  
+    $imagesize = getimagesize($this->file);
+    if(!is_array($imagesize) || empty($imagesize))
+      return false;
+    else
+      return true;
   }
 
   public function create_empty_image($width, $height){
@@ -109,6 +134,10 @@ class canvas{
 
   public function save($to = '', $quality = 100){
   
+  }
+
+  public function error_message(){
+    return $this->error;
   }
 
 }
