7a3b76d4aa189a13a33839af11c9acd8035acac7
Davi Ferreira <contato@daviferreira.com>
2011-07-12 12:53:20 -0300

    Save tests.

--- tests/index.php
+++ tests/index.php
@@ -5,8 +5,9 @@ require_once '../canvas.php';
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests for class Canvas');
-        $this->addFile(dirname(__FILE__).'/load_and_create_test.php');
-        $this->addFile(dirname(__FILE__).'/colors_test.php');
-        $this->addFile(dirname(__FILE__).'/resize_test.php');
+        #$this->addFile(dirname(__FILE__).'/load_and_create_test.php');
+        #$this->addFile(dirname(__FILE__).'/colors_test.php');
+        #$this->addFile(dirname(__FILE__).'/resize_test.php');
+        $this->addFile(dirname(__FILE__).'/save_and_show_test.php');
     }
 }
