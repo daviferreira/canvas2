5986c34f740cd8882a56a61fbdc43af0368e5352
Davi Ferreira <contato@daviferreira.com>
2011-07-19 17:32:19 -0300

    New readme

---
+++ README.markdown
@@ -0,0 +1,8 @@
+# Canvas 2
+## PHP image manipulation class
+
+This is a complete rewrite of the [original canvas class](http://www.daviferreira.com/posts/canvas-nova-classe-para-manipulacao-e-redimensionamento-de-imagens-com-php). As you can see, the code is now in english.
+
+## Coming soon
+
+Site, docs and examples. Stay tuned!
--- tests/index.php
+++ tests/index.php
@@ -9,12 +9,12 @@ define('BASE_URL', 'http://localhost/canvas2/');
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests for class Canvas');
-        #$this->addFile(dirname(__FILE__).'/load_and_create_test.php');
-        #$this->addFile(dirname(__FILE__).'/colors_test.php');
-        #$this->addFile(dirname(__FILE__).'/resize_test.php');
+        $this->addFile(dirname(__FILE__).'/load_and_create_test.php');
+        $this->addFile(dirname(__FILE__).'/colors_test.php');
+        $this->addFile(dirname(__FILE__).'/resize_test.php');
         $this->addFile(dirname(__FILE__).'/effects_test.php');
-        #$this->addFile(dirname(__FILE__).'/text_test.php');
-        #$this->addFile(dirname(__FILE__).'/save_test.php');
-        #$this->addFile(dirname(__FILE__).'/show_test.php');
+        $this->addFile(dirname(__FILE__).'/text_test.php');
+        $this->addFile(dirname(__FILE__).'/save_test.php');
+        $this->addFile(dirname(__FILE__).'/show_test.php');
     }
 }
